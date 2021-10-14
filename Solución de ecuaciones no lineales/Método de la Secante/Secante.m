2516688301fe38964abfa2798639289842a15c77
Juan Pablo Perez <91905676+JuanPablo-Perez@users.noreply.github.com>
2021-10-14 17:24:41 -0500

    Update Secante.m

--- Solución de ecuaciones no lineales/Método de la Secante/Secante.m
+++ Solución de ecuaciones no lineales/Método de la Secante/Secante.m
@@ -1,28 +1,45 @@
-
-% Funcion 
-function [raiz, datos] = Secante(funcion, intervalo, n_iteraciones)
+%Secante    Raices de ecuaciones (didáctica).
+%   Secante(funcion)
+%       [raiz,datos] = Secante(funcion, intervalo, n_iteraciones, tolerancia)
+%       raiz = raiz de la función
+%       datos = todas las variables de la ejecución
+%       funcion = la función a tratar 
+%       intervalo = [a, b]
+%       tolerancia = tolerancia a considerar
+%       n_iteraciones = número máximo de iteraciones
+%
+function [raiz, datos] = Secante(funcion, intervalo, n_iteraciones, tolerancia = 0.00001)
   format long;
-  syms x;
-  a = intervalo(1);
-  b = intervalo(2);
-  % Evaluar la funcion
-  Fa = double(subs(funcion, x, a));
-  Fb = double(subs(funcion, x, b));
-  
-  if Fb*Fa > 0
-    raiz = "El intervalo introducido no cumple Fa*Fb < 0";
-    datos = [];
-    return
-  endif
+  a = intervalo(1)
+  b = intervalo(2)
   datos = [];
-  for i = 1 : n_iteraciones
-    Fa = double(subs(funcion, x, a));
-    Fb = double(subs(funcion, x, b));
-    c = b - (Fb*(a-b))/(Fa-Fb);
-    datos = cat ( 1 , datos, c);
-    a = b;
-    b = c;
-  endfor
-  raiz = c;
+  i = 1;
+  while 1
+    disp(strcat("Numero iteracion: "))
+    disp(a);
+    disp(b);
+    Fa = funcion(a)
+    Fb = funcion(b)
+    raiz = b - (Fb*(a-b))/(Fa-Fb);
   
-  return
+    if Fb*Fa > 0
+      msgID = ' SECANTE:condicion';
+      msg = "El intervalo introducido no cumple Fa*Fb < 0";
+      error(msgID,msg)
+      
+    else if i > n_iteraciones
+      msgID = 'SECANTE:iteraciones';
+      msg = 'Gran número de iteraciones, posiblemente el método divergió';
+      error(msgID,msg)
+      
+    else
+      if abs(raiz-a)<tolerancia
+        break;
+      endif
+     endif
+      datos = cat ( 1 , datos, raiz);
+      a = b;
+      b = raiz;
+    endif
+    i=i+1;
+  end;
