2d984dd00f5a69355af6213f7122607efd57adde
Juan Pablo Perez <91905676+JuanPablo-Perez@users.noreply.github.com>
2021-10-14 15:28:33 -0500

    Add files via upload

---
+++ Solución de ecuaciones no lineales/Método Newton-Raphson/Newton.m
@@ -0,0 +1,13 @@
+function raiz = Newton(f,n,x1)
+  syms x
+  for i=1:n
+    df = diff(f);
+    f2=function_handle(f);
+    df2=function_handle(df);
+    x2=x1-(f2(x1)/df2(x1));
+    e=abs((x2-x1)/x2);
+    formatSpec = 'Con %1.0f iteraciones la raiz es %4.5f con un error de %4.5f\n';
+    fprintf(formatSpec,i,x2,e)
+    x1=x2;
+  endfor
+  raiz = x2
