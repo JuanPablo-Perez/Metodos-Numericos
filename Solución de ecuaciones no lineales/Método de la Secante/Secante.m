9ad79e5c926edc25f14dd62156b8cb385b1cca38
FranciscoCedillo <92124257+FranciscoCedillo@users.noreply.github.com>
2021-10-14 09:15:31 -0500

    Add files via upload

---
+++ Solución de ecuaciones no lineales/Método de la Secante/Secante.m
@@ -0,0 +1,28 @@
+
+% Funcion 
+function [raiz, datos] = Secante(funcion, intervalo, n_iteraciones)
+  format long;
+  syms x;
+  a = intervalo(1);
+  b = intervalo(2);
+  % Evaluar la funcion
+  Fa = double(subs(funcion, x, a));
+  Fb = double(subs(funcion, x, b));
+  
+  if Fb*Fa > 0
+    raiz = "El intervalo introducido no cumple Fa*Fb < 0";
+    datos = [];
+    return
+  endif
+  datos = [];
+  for i = 1 : n_iteraciones
+    Fa = double(subs(funcion, x, a));
+    Fb = double(subs(funcion, x, b));
+    c = b - (Fb*(a-b))/(Fa-Fb);
+    datos = cat ( 1 , datos, c);
+    a = b;
+    b = c;
+  endfor
+  raiz = c;
+  
+  return
