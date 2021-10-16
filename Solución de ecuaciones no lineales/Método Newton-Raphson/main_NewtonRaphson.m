a71888f303ff1bf3a87c08d3f92fd3d3df9a2027
Andres2800 <92129319+Andres2800@users.noreply.github.com>
2021-10-16 17:15:13 -0500

    Update main_NewtonRaphson.m

--- Solución de ecuaciones no lineales/Método Newton-Raphson/main_NewtonRaphson.m
+++ Solución de ecuaciones no lineales/Método Newton-Raphson/main_NewtonRaphson.m
@@ -1,24 +1,40 @@
 %Problema 1 
-%Se busca la potencia de una antena cuya funciÛn  est· dada por ls ecuaciÛn:
+%Se busca la potencia de una antena cuya función  está dada por ls ecuación:
 syms x
 fprintf("Problema 1 \n")
 f =  x-exp(-x);
 a = -2;
 b = 3;
 c = 1;
+datos="";
 
-% i nos indica el n√∫mero de iteraciones
-i = 3;
 
-r = Newton(f,i,c);
-figura1 = figure(1);
+[r, datos, n]= Newton(f,a,b,c);
+formatSpec = 'El valor de corte es %4.6f, el número de iteraciones es %1.0f\n';
+fprintf(formatSpec,r,n)
+
+  [m,o] = size(datos);
+  fprintf('Interación\ta\tb\txi\txi+1\tf(xi+1)\terror\n');
+  for i=1:m
+    fprintf('%d\t\t',i);
+    for j=1:o
+      fprintf('%d\t',datos(i,j));
+    endfor
+    fprintf('\n');
+  endfor
+
+  
+ %Gráfica del problema 1 
+  figura1 = figure(1);
 t = a:0.1:b;
 f2=function_handle(f);
 plot(t,f2(t), 'g-', "linewidth", 1)
 hold on;
 grid on;
+%Línea x = 0
 lineaCero = 0.*ones(length(t));
 plot(t, lineaCero, 'k-', "linewidth", 1);
+%Línea de corte
 Fa = double(subs(f, x, a));
 Fb = double(subs(f, x, b));
 yIntervalo =Fa: (Fb-Fa)/i: Fb;
@@ -27,26 +43,41 @@ plot(r.*ones(length(yIntervalo)), yIntervalo, 'r-', "linewidth", 1);
 
 
 %Problema 2
-%Se busca la distancia a la que da mayor seÒal una antena cuya ecuaciÛn es:
+%Se busca la distancia a la que da mayor señal una antena cuya ecuación es:
 fprintf("\n\nProblema 2 \n")
-f3 =  x.^2-3.*x-3;
-a2 = 0;
-b2 = 5;
-c2 = 3;
+f =  x.^2-3.*x-3;
+a = 0;
+b = 5;
+c = 3;
+[r, datos, n]= Newton(f,a,b,c);
+formatSpec = 'El valor de corte es %4.6f, el número de iteraciones es %1.0f\n';
+fprintf(formatSpec,r,n)
 
-% i2 nos indica el n√∫mero de iteraciones
-i2 = 3;
+  [m,o] = size(datos);
+  fprintf('Interación\ta\tb\txi\txi+1\tf(xi+1)\terror\n');
+  for i=1:m
+    fprintf('%d\t\t',i);
+    for j=1:o
+      fprintf('%d\t',datos(i,j));
+    endfor
+    fprintf('\n');
+  endfor
 
-r2 = Newton(f3,i2,c2);
+  
+
+
+%Gráfica del problema 2
 figura2 = figure(2);
-t2 = a2:0.1:b2;
-f4=function_handle(f3);
-plot(t2,f4(t2), 'b-', "linewidth", 1)
+t = a:0.1:b;
+f2 = function_handle(f);
+plot(t,f2(t), 'b-', "linewidth", 1)
 hold on;
 grid on;
-lineaCero = 0.*ones(length(t2));
-plot(t2, lineaCero, 'k-', "linewidth", 1);
-Fa = double(subs(f3, x, a2));
-Fb = double(subs(f3, x, b2));
+%Línea x = 0
+lineaCero = 0.*ones(length(t));
+plot(t, lineaCero, 'k-', "linewidth", 1);
+%Línea de corte
+Fa = double(subs(f, x, a));
+Fb = double(subs(f, x, b));
 yIntervalo =Fa: (Fb-Fa)/i: Fb;
-plot(r2.*ones(length(yIntervalo)), yIntervalo, 'r-', "linewidth", 1);
+plot(r.*ones(length(yIntervalo)), yIntervalo, 'r-', "linewidth", 1);
