35291d26f0090d3e7ee859df6525f6cabbef7c50
Andres2800 <92129319+Andres2800@users.noreply.github.com>
2021-10-16 17:17:13 -0500

    Update main_puntofijo.m

--- Solución de ecuaciones no lineales/Método Punto Fijo/main_puntofijo.m
+++ Solución de ecuaciones no lineales/Método Punto Fijo/main_puntofijo.m
@@ -1,7 +1,5 @@
-%Problema 1 
-%Problema 1: Se busca la distancia que tiene que estar un receptor de seÒal de una antena para
-% que esta sea maxÌma cuya ecuaciÛn est· dada por:
-syms x
+%Problema 1: Se busca la distancia que tiene que estar un receptor de señal de una antena para
+% que esta sea maxíma cuya ecuación está dada por:
 fprintf("Problema 1 \n")
 f = @(x) 2*exp(x)-x-10;
 g = @(x) 2*exp(x)-10;
@@ -9,43 +7,75 @@ a = -20;
 b = 0;
 c = -5;
 i = 5;
-r = Puntofijo(g,i,c)
+[r,datos,n] = Puntofijo(f,g,a,b,c);
+formatSpec = 'El valor de corte es %4.6f, el número de iteraciones es %1.0f\n';
+fprintf(formatSpec,r,n)
+
+  [m,o] = size(datos);
+  fprintf('Interación\ta\tb\txi\txi+1\tf(xi+1)\terror\n');
+  for i=1:m
+    fprintf('%d\t\t',i);
+    for j=1:o
+      fprintf('%d\t',datos(i,j));
+    endfor
+    fprintf('\n');
+  endfor
+
+   %Gráfica del problema 1 
 figura1 = figure(1);
 t = a:0.1:b;
 plot(t,f(t), 'b-', "linewidth", 1)
 hold on;
 grid on;
+%Línea x = 0
 lineaCero = 0.*ones(length(t));
 plot(t, lineaCero, 'k-', "linewidth", 1);
-Fa = double(subs(f, x, a));
-Fb = double(subs(f, x, b));
-yIntervalo =Fa: (Fb-Fa)/i: Fb;
+%Línea de corte
+Fa = f(a);
+Fb = f(b);
+yIntervalo = Fa: (Fb-Fa)/i: Fb;
 plot(r.*ones(length(yIntervalo)), yIntervalo, 'r-', "linewidth", 1);
 
 
 
 %Problema 2
-%Problema 2: Se busca la potencia necesaria de una antena para dar seÒal a toda una ciuda
-%en funciÛn de la corriente que le llega cuya ecuaciÛn esta dada por:
+%Problema 2: Se busca la potencia necesaria de una antena para dar señal a toda una ciuda
+%en función de la corriente que le llega cuya ecuación esta dada por:
 fprintf("\n\nProblema 2 \n")
-f2 = @(x) 2*x.^2-4.*x-1;
-g2 = @(x) sqrt((1+4*x)/2);
-a2 = 0;
-b2 = 3;
-c2 = 2;
-i2 = 10;
-r2 = Puntofijo(g2,i2,c2)
+f = @(x) 2*x.^2-4.*x-1;
+g = @(x) sqrt((1+4*x)/2);
+a = 0;
+b = 3;
+c = 2;
+i = 10;
+[r,datos,n] = Puntofijo(f,g,a,b,c);
+formatSpec = 'El valor de corte es %4.6f, el número de iteraciones es %1.0f\n';
+fprintf(formatSpec,r,n)
+
+  [m,o] = size(datos);
+  fprintf('Interación\ta\tb\txi\txi+1\tf(xi+1)\terror\n');
+  for i=1:m
+    fprintf('%d\t\t',i);
+    for j=1:o
+      fprintf('%d\t',datos(i,j));
+    endfor
+    fprintf('\n');
+  endfor
+
+%Gráfico problema 2
 figura2 = figure(2);
-t2 = a2:0.1:b2;
-plot(t2,f2(t2), 'g-', "linewidth", 1)
+t = a:0.1:b;
+plot(t,f(t), 'g-', "linewidth", 1)
 hold on;
 grid on;
-lineaCero = 0.*ones(length(t2));
-plot(t2, lineaCero, 'k-', "linewidth", 1);
-Fa = double(subs(f2, x, a2));
-Fb = double(subs(f2, x, b2));
+%Línea x = 0
+lineaCero = 0.*ones(length(t));
+plot(t, lineaCero, 'k-', "linewidth", 1);
+%Línea de corte
+Fa = f(a);
+Fb = f(b);
 yIntervalo =Fa: (Fb-Fa)/i: Fb;
-plot(r2.*ones(length(yIntervalo)), yIntervalo, 'r-', "linewidth", 1);
+plot(r.*ones(length(yIntervalo)), yIntervalo, 'r-', "linewidth", 1);
 
 
 
