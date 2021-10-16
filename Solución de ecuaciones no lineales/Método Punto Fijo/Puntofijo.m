3c61ac1581b9c98bc5381e949f3d7355382f9177
Andres2800 <92129319+Andres2800@users.noreply.github.com>
2021-10-16 17:16:24 -0500

    Update Puntofijo.m

--- Solución de ecuaciones no lineales/Método Punto Fijo/Puntofijo.m
+++ Solución de ecuaciones no lineales/Método Punto Fijo/Puntofijo.m
@@ -1,10 +1,43 @@
-function raiz = Puntofijo(f,n,x1)
-  format long
-  for i=1:n
-   x2 = f(x1);
-   e = abs((x2-x1)/x2);
-   formatSpec = 'Con %1.0f iteraciones la raiz es %4.5f con un error de %4.5f\n';
-   fprintf(formatSpec,i,x2,e)
-   x1=x2;
-  endfor
-  raiz = x2
+%Puntofijo    Raices de ecuaciones (didáctica).
+%   Puntofijo(funcion)
+%       [raiz,datos,i] = Puntofijo(f,g,a,b,c,tol,iter)
+%       raiz = raiz de la función
+%       datos = todas las variables de la ejecución
+%       i = número de iteraciones 
+%       f = la función a tratar 
+%       g = función de la forma x=g(x)
+%       [a, b] = intervalo 
+%       c = punto de inicio 
+%       tol = tolerancia a considerar
+%       iter = número máximo de iteraciones
+
+function [raiz,datos,i] = Puntofijo(f,g,a,b,c,tol=0.0001,iter=100)
+i = 1;
+  while 1
+    fa = f(a);
+    fb = f(b);
+    raiz = g(c);
+    e = abs((raiz-c)/(c));
+    datos(i,1)=a;
+    datos(i,2)=b;
+    datos(i,3)=c;
+    datos(i,4)=raiz;
+    datos(i,5)=f(raiz);
+    datos(i, 6)=e;
+    c = raiz;
+     if (fa*fb>0)
+      msgID = ' Punto fijo:condicion';
+      msg = "El intervalo introducido no cumple Fa*Fb < 0";
+      error(msgID,msg)
+    elseif i>iter
+      msgID = 'Punto fijo:iteraciones';
+      msg = 'Gran número de iteraciones, posiblemente el método divergió';
+      error(msgID,msg)
+    else
+        if e < tol
+            break;
+        end
+    end
+    i=i+1;
+  endwhile
+end
