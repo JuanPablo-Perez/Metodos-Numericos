3c7d12c162d0c7c4d0239083ad6a484b4aaa12b6
Andres2800 <92129319+Andres2800@users.noreply.github.com>
2021-10-16 17:14:11 -0500

    Update Newton.m

--- Solución de ecuaciones no lineales/Método Newton-Raphson/Newton.m
+++ Solución de ecuaciones no lineales/Método Newton-Raphson/Newton.m
@@ -1,13 +1,46 @@
-function raiz = Newton(f,n,x1)
+%Newton    Raices de ecuaciones (didáctica).
+%   Newton(funcion)
+%       [raiz,datos,i] = Newton(f,a,b,c,tol,iter)
+%       raiz = raiz de la función
+%       datos = todas las variables de la ejecución
+%       i = número de iteraciones 
+%       f = la función a tratar 
+%       [a, b] = intervalo 
+%       c = punto de inicio 
+%       tol = tolerancia a considerar
+%       iter = número máximo de iteraciones
+
+function [raiz,datos,i] = Newton(f,a,b,c,tol=0.000001,iter=100)
   syms x
-  for i=1:n
-    df = diff(f);
-    f2=function_handle(f);
-    df2=function_handle(df);
-    x2=x1-(f2(x1)/df2(x1));
-    e=abs((x2-x1)/x2);
-    formatSpec = 'Con %1.0f iteraciones la raiz es %4.5f con un error de %4.5f\n';
-    fprintf(formatSpec,i,x2,e)
-    x1=x2;
-  endfor
-  raiz = x2
+  i = 1;
+  df = diff(f);
+  f2 = function_handle(f);
+  df2 = function_handle(df);
+  while 1
+    fa = f2(a);
+    fb = f2(b);
+    raiz = c-(f2(c))/(df2(c));
+    e = abs((raiz-c)/(c));
+    datos(i,1)=a;
+    datos(i,2)=b;
+    datos(i,3)=c;
+    datos(i,4)=raiz;
+    datos(i,5)=f2(raiz);
+    datos(i, 6)=e;
+    c = raiz;
+     if (fa*fb>0)
+      msgID = ' Newton-Raphson:condicion';
+      msg = "El intervalo introducido no cumple Fa*Fb < 0";
+      error(msgID,msg)
+    elseif i>iter
+      msgID = 'Newton-Raphson:iteraciones';
+      msg = 'Gran número de iteraciones, posiblemente el método divergió';
+      error(msgID,msg)
+    else
+        if e < tol
+            break;
+        end
+    end
+    i=i+1;
+  endwhile
+end
