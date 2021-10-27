6970c1a6c8c24feb59277395875ffa8c6c46b548
Juan Pablo Perez <91905676+JuanPablo-Perez@users.noreply.github.com>
2021-10-27 17:20:28 -0500

    Add files via upload

---
+++ Soluciones a sistemas de ecuaciones lineales/Gauss/gauss.m
@@ -0,0 +1,39 @@
+%Gauss   (didáctica).
+%       [Aesc, solucion] = gauss(A,b) 
+%       Parámetros entrada
+%            A = matriz cuadrada de coeficientes
+%            b = matriz de términos independientes
+%        Parámetros salida
+%             Aesc =  matriz A escalonada 
+%             solucion = vector solución de A
+
+
+function  [Aesc,solucion]=gauss  (A, b);
+  [q, w] = size(A);
+  if q!= w
+    msgID = 'Eliminiación Gaussiana: La matriz no es cuadrada';
+    msg = 'La matriz no es cuadrada';
+    error(msgID,msg)
+  endif
+  n = length(b);
+  for j = 1:n-1;
+    for k = j+1:n;
+      al = -A(k,j)/A(j,j);
+      for m = j:n;
+        A(k,m) = A(k,m)+al*A(j,m);
+      end
+      b(k) = b(k)+al*b(j);
+    end  
+  end
+  Aesc = cat(2, A, b);
+  % Reemplazo de variables
+[n n]=size(A);
+A=[A';b']';
+x=zeros(n,1);
+for p=1:n
+    for k=[1:p-1,p+1:n];
+    m=-A(k,p)/A(p,p);
+    A(k,:)=A(k,:)+m*A(p,:);
+end
+solucion= A(:,n+1)./diag(A);
+end
---
+++ Soluciones a sistemas de ecuaciones lineales/Gauss/main_Gauss.m
@@ -0,0 +1,58 @@
+%Autores : Carlos Calle, Francisco Cedillo y Juan Pérez
+%
+%Fecha : 20/10/2021
+%Descripción : El programa tiene la finalidad de resolver
+%                                 sistemas de ecuaciones lineales por el 
+%                                 método de eliminación de Gauss
+
+%                  Problema 1
+%Halle el potencial en a,b,c,d si luego de realizar el análisis
+%nodal tenemos el siguiente sistema de ecuaciones:
+%   -7a   +   2b  +    0     + 4d  = -60 
+%       a    -   4b  +   2c   +   0   = -90 
+%      0     -   5b  +  11c  -  2d  =  0   
+%     5a   +     0   +   2c   -  8d  = 300 
+A= [-7, 2, 0, 4;  1, -4, 2, 0; 0, -5, 11, -2; 5, 0, 2, -8];
+b = [-60; -90; 0; 300];
+disp("PROBLEMA 1")
+try
+  [A_escalonada, solucion]=gauss(A, b);
+  disp("La matriz  escalonada es:   ");
+  disp(A_escalonada);
+  
+  disp("Los resultados son; ");
+  for i=1:length(solucion);
+    textoResultado = strcat("x", mat2str(i), " = ", mat2str(solucion(i)));
+    disp(textoResultado)
+  endfor
+  
+catch err
+  fprintf('Error: %s\n',err.message);
+end
+
+
+%                   Problema 2
+% Encuentre las corrientes i1, i2, i3, i4, Si lsego de realizar el 
+% análisis obtenemos el sistema de ecuaciones:
+%   7i1   -  2i2   +    0     +      0 = 8
+% -2i1   +  6i2   -  4i3  +      0 =  4
+%     0     -  4i2  +   5i3  -     i4  = - 10
+%     0     +     0    -    i3    +  5i4 = -4
+A= [7, -2, 0, 0;  2, 6, -4, 0; 0, -4, 5, -1; 0, 0, -1, 5];
+b = [8 ;4 ;-10; -4];
+disp('')
+disp("PROBLEMA 2")
+try
+  [A_escalonada, solucion]=gauss(A, b);
+  disp("La matriz  escalonada es:   ");
+  disp(A_escalonada);
+  
+  disp("Los resultados son; ");
+  for i=1:length(solucion);
+    textoResultado = strcat("x", mat2str(i), " = ", mat2str(solucion(i)));
+    disp(textoResultado)
+  endfor
+  
+catch err
+  fprintf('Error: %s\n',err.message);
+end
