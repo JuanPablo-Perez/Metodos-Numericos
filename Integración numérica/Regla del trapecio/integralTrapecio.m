29b67b7f9ebb20ed7036cbda5e066599e8f2ccc5
Juan Pablo Perez <91905676+JuanPablo-Perez@users.noreply.github.com>
2021-11-19 08:13:08 -0500

    Add files via upload

---
+++ Integración numérica/Regla del trapecio/integralTrapecio.m
@@ -0,0 +1,10 @@
+
+
+function [valor, error] = integralTrapecio(funcion, intervalo)
+  a = intervalo(1);
+  b = intervalo(2);
+  integralReal = integral(funcion, a, b);
+  valor = double((b-a)*(funcion(a)+funcion(b))/2);
+  error = abs(valor - integralReal)*100/integralReal;
+endfunction
+  
---
+++ Integración numérica/Regla del trapecio/main_integralTrapecio.m
@@ -0,0 +1,19 @@
+funcion  = @(x)0.2 + 25*x -200*x.^2 + 675*x.^3 -900*x.^4 + 400*x.^5;
+intervalo = [0, 0.8];
+[valor, error] = integralTrapecio(funcion, intervalo)
+
+figura1 = figure(1);
+t = linspace(intervalo(1), intervalo(2), 100);
+fa  = funcion(intervalo(1));
+fb = funcion(intervalo(2));
+X = linspace(intervalo(1), intervalo(2), 100);
+Y = linspace(fa, fb, 100);
+
+plot(intervalo, [fa, fb], 'b-' )
+surf([X; X], [Y(1,:); 0*Y(1,:)], 0*[zeros(size(X)); ones(size(X))])
+hold on 
+grid on
+view(0, 90) ;
+plot(t, funcion(t), 'r');
+
+
