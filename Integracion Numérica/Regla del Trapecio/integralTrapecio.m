3b72d9d3d9c5672b0bd125682d876e3d072cff99
FranciscoCedillo <92124257+FranciscoCedillo@users.noreply.github.com>
2021-12-01 12:57:33 -0500

    Add files via upload

---
+++ Integracion Numérica/Regla del Trapecio/integralTrapecio.m
@@ -0,0 +1,37 @@
+%integralTrapacio
+%       [valor, error,datosGrafica] = ntdiferenciasDivididas_Adelante(f, xi, h);
+%
+%       Parámetros entrada
+%            funcion= función simbólica
+%            intervalo = Intervalo donde se evaluará la integral
+%            n = segmentos en el que se dividirá el intervalo
+%
+%        Parámetros salida
+%             valor = valor aproximado de la integral
+%             error = porcentaje de error entre la integral real, y aquella calculada
+%             datosGrafica = datos que nos ayudaran a realizar la grafica de la función
+
+
+function [valor, error, datosGrafica] = integralTrapecio(funcion, intervalo, n)
+  n = n-1;
+  a = intervalo(1);
+  b = intervalo(2);
+  sum = funcion(a);
+  h = (b-a)/n;
+  datosGrafica = [];
+  x = [a];
+  y = [funcion(a)];
+  for i = a+h:h:b-h;
+    sum = sum+2*funcion(i);
+    y = cat(1, y, funcion(i));
+    x = cat(1, x, i);
+  endfor
+  x = cat(1, x, b);
+  y = cat(1, y, funcion(b));
+  sum = sum + funcion(b);
+  valor = (h)*sum/2;
+  integralReal = integral(funcion, a, b);
+  error = abs(valor - integralReal)*100/integralReal;
+  datosGrafica = [x , y];
+endfunction
+  
